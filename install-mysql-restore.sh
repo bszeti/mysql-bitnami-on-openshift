0aac0da462e906b934a2fd77fa6d6d92e59df61a
Balazs Szeti <szeti.balazs@gmail.com>
2026-04-02 12:42:47 -0400

    Restore in another namespace

--- install-mysql-restore.sh
+++ install-mysql-restore.sh
@@ -1,28 +1,29 @@
-# Secret for GCP credentials
-# oc apply -f deploy-mysql/secret-gcp-credentials.yaml
-
 # MySQL instance name. Also update MYSQL_SERVICE in deploy-mysql/job-restore.yaml
-INSTANCE=restore
+INSTANCE=test
+NAMESPACE=mysql-restore
 
-# Delete CronJob for mysqldump backups during restore
-# oc delete -f deploy-mysql/cronjob-backup-mysqldump.yaml
+# Create namespace 
+# oc new-project $NAMESPACE
+
+# Secret for GCP credentials
+# oc apply -n $NAMESPACE -f deploy-mysql/secret-gcp-credentials.yaml
 
 # ConfigMap with scripts used in secondary node sidecars
-oc apply -f deploy-mysql/scripts-sidecars.yaml
+oc apply -n $NAMESPACE -f deploy-mysql/scripts-sidecars.yaml
 
 # Install MySQL with bin backup sidecars
-helm upgrade -i --wait --timeout=5m $INSTANCE oci://registry-1.docker.io/bitnamicharts/mysql -f deploy-mysql/values-restore.yaml
+helm upgrade -i -n $NAMESPACE --wait --timeout=5m $INSTANCE oci://registry-1.docker.io/bitnamicharts/mysql -f deploy-mysql/values-restore.yaml
 
 # Create job to restore databases from backup
-oc delete -f deploy-mysql/job-restore.yaml; oc create -f deploy-mysql/job-restore.yaml
-oc wait --for=jsonpath='{.status.ready}'=1 job/restore
-oc logs -f -c download job/restore
-oc logs -f -c restore job/restore
+oc delete -n $NAMESPACE -f deploy-mysql/job-restore.yaml; oc create -f deploy-mysql/job-restore.yaml
+oc wait -n $NAMESPACE --for=jsonpath='{.status.ready}'=1 job/restore
+oc logs -n $NAMESPACE -f -c download job/restore
+oc logs -n $NAMESPACE -f -c restore job/restore
 
 # Verify restored db
-oc exec -c mysql $INSTANCE-mysql-secondary-0 -- /bin/bash -c 'mysql -u root -p$(cat $MYSQL_MASTER_ROOT_PASSWORD_FILE) --vertical <<<"SHOW REPLICA STATUS;"'
-oc exec -c mysql $INSTANCE-mysql-secondary-0 -- /bin/bash -c 'mysql -u root -p$(cat $MYSQL_MASTER_ROOT_PASSWORD_FILE) db1 <<<"SELECT count(*) FROM messages; SELECT * FROM messages ORDER BY created_at DESC LIMIT 1;"'
-oc exec -c mysql $INSTANCE-mysql-secondary-0 -- /bin/bash -c 'mysql -u root -p$(cat $MYSQL_MASTER_ROOT_PASSWORD_FILE) db1 <<<"
+oc exec -n $NAMESPACE -c mysql $INSTANCE-mysql-secondary-0 -- /bin/bash -c 'mysql -u root -p$(cat $MYSQL_MASTER_ROOT_PASSWORD_FILE) --vertical <<<"SHOW REPLICA STATUS;"'
+oc exec -n $NAMESPACE -c mysql $INSTANCE-mysql-secondary-0 -- /bin/bash -c 'mysql -u root -p$(cat $MYSQL_MASTER_ROOT_PASSWORD_FILE) db1 <<<"SELECT count(*) FROM messages; SELECT * FROM messages ORDER BY created_at DESC LIMIT 1;"'
+oc exec -n $NAMESPACE -c mysql $INSTANCE-mysql-secondary-0 -- /bin/bash -c 'mysql -u root -p$(cat $MYSQL_MASTER_ROOT_PASSWORD_FILE) db1 <<<"
     SELECT 
         table_schema AS '\''Db'\'', 
         ROUND(SUM(data_length + index_length) / 1024 / 1024 / 1024, 2) AS '\''Size (GB)'\''
@@ -30,11 +31,14 @@ oc exec -c mysql $INSTANCE-mysql-secondary-0 -- /bin/bash -c 'mysql -u root -p$(
     WHERE table_schema = DATABASE();
     "'
 
-# Create CronJob for mysqldump backups
-# oc apply -f deploy-mysql/cronjob-backup-mysqldump.yaml
-
-
+# Create CronJob for mysqldump backups for restored database
+oc apply -n $NAMESPACE -f deploy-mysql/cronjob-backup-mysqldump.yaml
+# Force run
+job=$(oc create job -oname manual-backup-mysqldump-$(date +%Y%m%d%H%M%S) --from=cronjob/backup-mysqldump)
+oc wait --for=jsonpath='{.status.ready}'=1 $job
+oc logs -f -c mysqldump $job
+oc logs -f -c upload $job
 
 # Uninstall
-# helm uninstall -n mysql restore
-# oc delete -f deploy-mysql/cronjob-backup-mysqldump.yaml
+# helm uninstall -n $NAMESPACE test
+# oc delete -n $NAMESPACE -f deploy-mysql/cronjob-backup-mysqldump.yaml
